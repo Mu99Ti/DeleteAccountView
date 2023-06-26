a5b74ed7a0136f173e784c511ab21b46b907f2e5
Muhamad Talebi <muhamad79t@gmail.com>
2023-06-26 12:20:32 +0330

    Initial Commit

---
+++ DeleteAccountView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DeleteAccountView
+//
+//  Created by Muhamad Talebi on 6/26/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
